c124e4b54365bc2c5e5a710e1786c635f2ef7773
Shaheer Shukur <shaheershukur@gmail.com>
2024-04-19 15:27:22 +0400

    feat: updates config

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
+import { Button, Card, Text } from "@shaheershukur/react-ui-components";
 
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div className="flex flex-col gap-4 p-8">
+      <Card skin="outlined" roundness="large">
+        <Button skin="primary" size="large">
+          Button
+        </Button>
+        <Button skin="secondary" size="medium">
+          Button
+        </Button>
+        <Button skin="danger" size="small">
+          Button
+        </Button>
+      </Card>
+    </div>
+  );
 }

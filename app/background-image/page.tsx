9da71aa050289aa3ff716a7c9d7cfafef41243ab
Shaheer Shukur <shaheershukur@gmail.com>
2024-04-20 20:41:51 +0400

    feat: updates bg

--- app/background-image/page.tsx
+++ app/background-image/page.tsx
@@ -4,22 +4,25 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function BackgroundImage() {
-  const imageNumber = Math.floor(Math.random() * 6 + 1);
   const [imageSrc, setImageSrc] = useState("/bg_1.jpg");
 
   useEffect(() => {
-    setImageSrc(`/bg_${imageNumber}.jpg`);
-  }, [imageNumber]);
+    const interval = setInterval(() => {
+      const randomNumber = Math.floor(Math.random() * 6 + 1);
+      setImageSrc(`/bg_${randomNumber}.jpg`);
+    }, 3000);
+    return () => clearInterval(interval);
+  });
 
   return (
     <Image
-      src={imageSrc}
+      alt="Background Image"
+      className="animate-[pulse_1s_linear]"
       fill
-      placeholder="blur"
-      blurDataURL="/bg_1.jpg"
-      alt={`Background Image ${imageNumber} ${imageSrc}`}
-      style={{ objectFit: "cover" }}
+      key={imageSrc}
       priority
+      src={imageSrc}
+      style={{ objectFit: "cover" }}
     />
   );
 }
